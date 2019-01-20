431ce1f0463eb9911604bf47cde35c1bd8e93d1a
Junior Melgarejo <juniormelgarejo@jera.com.br>
2019-01-20 15:12:04 -0300

    Serializing image

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,15 @@
 class UserSerializer < ActiveModel::Serializer
 
-    attributes :id, :name, :email, :num_of_followers, :num_of_followings
+    attributes :id, :name, :email, :num_of_followers, :num_of_followings, :avatar
+
+    def avatar
+    return nil if object.avatar.nil?
+
+    {
+      mini_thumb: object.avatar.url,
+      thumb: object.avatar.url,
+      medium: object.avatar.url
+    }
+  end
 
 end
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -13,6 +13,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def default_url
+    "#{asset_host}/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
